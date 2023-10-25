b617e0b11e1e512bb1f736520ff3d257893226be
Lorcc <marchoffmann9719@gmail.com>
2023-10-26 00:14:05 +0200

    division of space in sections

--- Assets/SupermarketProject/Scripts/SetupSupermarket_old.cs
+++ Assets/SupermarketProject/Scripts/SetupSupermarket_old.cs
@@ -2,26 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SetupSupermarket : MonoBehaviour
+public class SetupSupermarket_old : MonoBehaviour
 {
-    [SerializeField] private int min_ground_size = 20;
-    [SerializeField] private int max_ground_size = 30;
+    [SerializeField] private int min_ground_size = 3;
+    [SerializeField] private int max_ground_size = 10;
 
-    [SerializeField] private int min_entrance_size = 6;
-    [SerializeField] private int max_entrance_size = 7;
+    [SerializeField] private int min_entrance_size = 4;
+    [SerializeField] private int max_entrance_size = 5;
 
     [SerializeField] private GameObject shelf_pref;
     [SerializeField] private GameObject entrance_pref;
-    [SerializeField] private GameObject fruits_pref;
-    [SerializeField] private GameObject durablefood_pref;
-    [SerializeField] private GameObject alcohol_pref;
-
-
-
     private List<GameObject> entrance_tiles = new List<GameObject>();
-    private List<GameObject> fruits_tiles = new List<GameObject>();
-    private List<GameObject> durablefood_tiles = new List<GameObject>();
-    private List<GameObject> alcohol_tiles = new List<GameObject>();
     private List<GameObject> shelve_tiles = new List<GameObject>();
 
 
@@ -36,37 +27,15 @@ public class SetupSupermarket : MonoBehaviour
         Debug.Log("Grid in y: " + grid_size_y);
 
         // Generate entrance
-        Quaternion entranceRotation = Quaternion.Euler(0, 0, 0);
+        Quaternion objectRotation = Quaternion.Euler(0, 0, 0);
         entrance_pref.transform.localScale = new Vector3(Random.Range(min_entrance_size, max_entrance_size), 0.5f, Random.Range(min_entrance_size, max_entrance_size));
-        Vector3 entrance_size = entrance_pref.transform.localScale;
-        Vector3 entrance_position = this.transform.localPosition + new Vector3((grid_size_x / 2.0f - entrance_size[0] / 2.0f), 0.5f, (-grid_size_y / 2.0f + entrance_size[2] / 2.0f));
-        GameObject entrance = Instantiate(entrance_pref, entrance_position, entranceRotation, this.transform);
+        Vector3 entrance_position = this.transform.localPosition + new Vector3((grid_size_x / 2.0f - entrance_pref.transform.localScale[0] / 2.0f), 0.5f, (-grid_size_y / 2.0f + entrance_pref.transform.localScale[2]/2.0f));
+        GameObject entrance = Instantiate(entrance_pref, entrance_position, objectRotation, this.transform);
         entrance_tiles.Add(entrance);
-
-        // Generate fruits vegetable 
-        Quaternion fruitsRotation = Quaternion.Euler(0, 0, 0);
-        fruits_pref.transform.localScale = new Vector3(entrance_size[0], 0.5f, grid_size_y - entrance_size[2]);
-        Vector3 fruits_position = this.transform.localPosition + new Vector3((grid_size_x / 2.0f - fruits_pref.transform.localScale[0] / 2.0f), 0.5f, (grid_size_y / 2.0f - fruits_pref.transform.localScale[2] / 2.0f));
-        GameObject fruits = Instantiate(fruits_pref, fruits_position, fruitsRotation, this.transform);
-        fruits_tiles.Add(fruits);
-
-        // Generate durablefood
-        Quaternion durablefoodRotation = Quaternion.Euler(0, 0, 0);
-        durablefood_pref.transform.localScale = new Vector3(grid_size_x - entrance_size[0], 0.5f, grid_size_y - entrance_size[2]);
-        Vector3 durablefood_position = this.transform.localPosition + new Vector3(( durablefood_pref.transform.localScale[0]/2.0f - grid_size_x / 2.0f), 0.5f, (grid_size_y / 2.0f - durablefood_pref.transform.localScale[2] / 2.0f));
-        GameObject durablefood = Instantiate(durablefood_pref, durablefood_position, durablefoodRotation, this.transform);
-        durablefood_tiles.Add(durablefood);
-
-        // Generate fruits vegetable 
-        Quaternion alcoholRotation = Quaternion.Euler(0, 0, 0);
-        alcohol_pref.transform.localScale = new Vector3(grid_size_x - entrance_size[0], 0.5f, entrance_size[2]);
-        Vector3 alcohol_position = this.transform.localPosition + new Vector3((alcohol_pref.transform.localScale[0] / 2.0f - grid_size_x / 2.0f), 0.5f, (-grid_size_y / 2.0f + alcohol_pref.transform.localScale[2] / 2.0f));
-        GameObject alcohol = Instantiate(alcohol_pref, alcohol_position, alcoholRotation, this.transform);
-        alcohol_tiles.Add(alcohol);
 
         // Generate north outershelves
         //bool[,] number_of_tiles = new bool[grid_size_x, grid_size_y];
-        /*
+
         for (int x = 1; x < grid_size_x; x++)
         {
             float offset = 0.5f;
@@ -101,6 +70,10 @@ public class SetupSupermarket : MonoBehaviour
             Quaternion shelve_rotation = Quaternion.Euler(0, 0, 0);
             GameObject shelve = Instantiate(shelf_pref, shelve_position, shelve_rotation, this.transform);
             shelve_tiles.Add(shelve);
+        }
+        /*for (int grid_vert = 1; grid_vert < grid_size_x; grid_vert++)
+        {
+
         }*/
     }
 
@@ -109,12 +82,12 @@ public class SetupSupermarket : MonoBehaviour
     void Start()
     {
         calculate_Grid();
-    }
+    } 
 
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
