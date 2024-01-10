a8be61dff87d406dbc17bbbf9e4cd439ec3cfbc2
Lorcc <marchoffmann9719@gmail.com>
2024-01-10 23:41:46 +0100

    added new purchable items

--- Assets/SupermarketProject/Scenes/FoodSpawner/PurchableFoodSpawner.cs
+++ Assets/SupermarketProject/Scenes/FoodSpawner/PurchableFoodSpawner.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class PurchableFoodSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject durableable_food_items;
-    [SerializeField] private GameObject beverages_items;
-    [SerializeField] private GameObject fruit_items;
-
+    [SerializeField] private GameObject[] durableable_food_items;
+    [SerializeField] private GameObject[] fruit_items;
+    [SerializeField] private GameObject[] beverages_items;
+    //TODO delete all_items at end
     private List<GameObject> all_items = new List<GameObject>();
 
     public void spawn_durable_item()
     {
-        GameObject new_object = Instantiate(durableable_food_items, this.transform);
-        all_items.Add(new_object);
-    }
-    public void spawn_drinks_item()
-    {
-        GameObject new_object = Instantiate(beverages_items, this.transform);
+        //Return a random int within [minInclusive..maxExclusive)
+        int random_item = Random.Range(0, durableable_food_items.Length);
+        GameObject new_object = Instantiate(durableable_food_items[random_item], this.transform);
         all_items.Add(new_object);
     }
     public void spawn_fruit_item()
     {
-        GameObject new_object = Instantiate(fruit_items, this.transform);
+        int random_item = Random.Range(0, fruit_items.Length);
+        GameObject new_object = Instantiate(fruit_items[random_item], this.transform);
+        all_items.Add(new_object);
+    }
+    public void spawn_drinks_item()
+    {
+        Debug.Log(beverages_items.Length);
+        int random_item = Random.Range(0, beverages_items.Length);
+        GameObject new_object = Instantiate(beverages_items[random_item], this.transform);
         all_items.Add(new_object);
     }
 }
