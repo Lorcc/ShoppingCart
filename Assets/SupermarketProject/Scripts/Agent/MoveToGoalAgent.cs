3417f43736714b227d861e09df424c1174c9c281
Lorcc <marchoffmann9719@gmail.com>
2024-01-24 13:30:48 +0100

    temp added basic "car" movement

--- Assets/SupermarketProject/Scripts/Agent/MoveToGoalAgent.cs
+++ Assets/SupermarketProject/Scripts/Agent/MoveToGoalAgent.cs
@@ -8,6 +8,16 @@ using Unity.MLAgents.Sensors;
 public class MoveToGoalAgent : Agent
 {
     [SerializeField] private Transform targetTransform;
+    [SerializeField] private GameObject agent_camera;
+    private float agent_cameraspeed = 100f;
+    private float agent_movespeed_force = 1500f;
+    private float agent_movespeed_velocity = 150f;
+    private float ground_drag = 5f;
+    private float ground_angular_drag = 5f;
+
+    Vector3 movement_direction;
+
+    Rigidbody agent_rigidbody;
 
     public override void OnEpisodeBegin()
     {
@@ -20,11 +30,34 @@ public class MoveToGoalAgent : Agent
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        float moveX = actions.ContinuousActions[0];
+        // old movement
+        /*float moveX = actions.ContinuousActions[0]; 
         float moveZ = actions.ContinuousActions[1];
 
         float moveSpeed = 5f;
         transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+        */
+
+        float movement_x = actions.ContinuousActions[1];
+        float rotation_y = actions.ContinuousActions[0];
+
+        Vector3 rotation_direction = new Vector3(0, rotation_y, 0);
+        Quaternion delta_rotation = Quaternion.Euler(rotation_direction * Time.fixedDeltaTime * agent_cameraspeed);
+        agent_rigidbody.MoveRotation(agent_rigidbody.rotation * delta_rotation);
+        //agent_rigidbody.angularDrag = ground_angular_drag;
+
+        Vector3 cam_f = agent_camera.transform.forward;
+        cam_f.y = 0;
+        cam_f = cam_f.normalized;
+
+        movement_direction = cam_f * movement_x;
+        //agent_rigidbody.velocity = movement_direction * Time.fixedDeltaTime * agent_movespeed_velocity;
+        agent_rigidbody.AddForce(movement_direction * Time.fixedDeltaTime * agent_movespeed_force, ForceMode.Force);
+
+        agent_rigidbody.drag = ground_drag;
+
+
+        
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -45,5 +78,13 @@ public class MoveToGoalAgent : Agent
         {
             AddReward(-0.5f);
         }
+    }
+
+
+
+    private void Start()
+    {
+        agent_rigidbody = GetComponent<Rigidbody>();
+        agent_rigidbody.freezeRotation = true;
     }
 }
